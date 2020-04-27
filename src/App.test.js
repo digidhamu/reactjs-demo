d9dccc0b37e84cfcd6f526036fa830e7dc6c1434
Dhamodharan Krishnan <dhamukrish@gmail.com>
2020-04-27 10:02:18 +0200

    Updated code coverage

--- src/App.test.js
+++ src/App.test.js
@@ -4,6 +4,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/DaaS Demo App/i);
+  const linkElement = getByText(/DaaS Demo - React App/i);
   expect(linkElement).toBeInTheDocument();
 });
