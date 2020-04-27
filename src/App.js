ce132faa338106398c4d89ba3b87226af91f660a
Dhamodharan Krishnan <dhamukrish@gmail.com>
2020-04-27 08:48:28 +0200

    Updated with material-ui

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Button from '@material-ui/core/Button';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h1>
+          DaaS Demo - React App
+        </h1>
+        <Button variant="contained" color="primary">
+          Home
+        </Button>
         <p>
-          DaaS Demo App
+          Version 0.0.1
         </p>
       </header>
     </div>
