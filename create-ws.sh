59799b4467895516e38cb69b97be08e93ea2aca3
Dhamodharan Krishnan <dhamukrish@gmail.com>
2020-05-02 12:42:33 +0200

    Cleanup workspace is added

--- create-ws.sh
+++ create-ws.sh
@@ -3,7 +3,6 @@
 # Normally, git-fetch job
 # Once pulled, other script start use this script
 ###############################################################################
-find . -mindepth 1 -exec rm -rf -- {} + 2>/dev/null
 git init
 git remote add origin git@github.com:digidhamu/reactjs-demo.git
 git fetch origin
