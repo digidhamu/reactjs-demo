8a04e6c4d4f3eba7ade9dc25472713584f462422
Dhamodharan Krishnan <dhamukrish@gmail.com>
2020-05-03 07:58:29 +0200

    Updated artifacts upload

--- artifact-upload.sh
+++ artifact-upload.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
 
-echo "Artifacts Upload"
+curl -v \
+    --user "admin:$1" \
+    --upload-file ./results/api-test-results.html https://art.daas.digidhamu.com/repository/raw-daas-digidhamu/test-results/api-test-results.html
+
+curl -v \
+    --user "admin:$1" \
+    --upload-file ./results/api-test-results.json https://art.daas.digidhamu.com/repository/raw-daas-digidhamu/test-results/api-test-results.json
+
+curl -v \
+    --user "admin:$1" \
+    --upload-file ./results/perf-test-results.txt https://art.daas.digidhamu.com/repository/raw-daas-digidhamu/test-results/perf-test-results.txt
+
+curl -v \
+    --user "admin:$1" \
+    --upload-file ./results/ux-test-results.html https://art.daas.digidhamu.com/repository/raw-daas-digidhamu/test-results/ux-test-results.html
