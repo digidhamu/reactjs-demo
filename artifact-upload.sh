e21a8831da6a52b7b498af5d04b0a37ff6bb0ce5
Dhamodharan Krishnan <dhamukrish@gmail.com>
2020-05-03 10:15:39 +0200

    artifact-upload is updated

--- artifact-upload.sh
+++ artifact-upload.sh
@@ -1,23 +1,62 @@
 #!/bin/bash
 
-curl -v \
-    --user "admin:$1" \
-    --upload-file ./results/api-test-results.html https://art.daas.digidhamu.com/repository/raw-daas-digidhamu/test-results/api-test-results.html
+export timestamp=`date +%Y-%m-%d"_"%H_%M_%S`
+
+# ART_PASS=$1
+ART_PASS=kannan123
+
+tar -czvf \
+    ./results_${timestamp}.tar.gz \
+    ./results/* \
+    || exit 1
 
 sleep 2
 
 curl -v \
-    --user "admin:$1" \
-    --upload-file ./results/api-test-results.json https://art.daas.digidhamu.com/repository/raw-daas-digidhamu/test-results/api-test-results.json
+    --user "admin:$ART_PASS" \
+    --upload-file ./results_${timestamp}.tar.gz \
+        https://art.daas.digidhamu.com/repository/raw-daas-digidhamu/test-results/results_${timestamp}.tar.gz \
+    || exit 1
 
 sleep 2
 
 curl -v \
-    --user "admin:$1" \
-    --upload-file ./results/perf-test-results.txt https://art.daas.digidhamu.com/repository/raw-daas-digidhamu/test-results/perf-test-results.txt
+    --user "admin:$ART_PASS" \
+    --upload-file ./results/api-test-results.html \
+        https://art.daas.digidhamu.com/repository/raw-daas-digidhamu/test-results/api-test-results.html \
+    || exit 1
 
 sleep 2
 
 curl -v \
-    --user "admin:$1" \
-    --upload-file ./results/ux-test-results.html https://art.daas.digidhamu.com/repository/raw-daas-digidhamu/test-results/ux-test-results.html
+    --user "admin:$ART_PASS" \
+    --upload-file ./results/api-test-results.json \
+        https://art.daas.digidhamu.com/repository/raw-daas-digidhamu/test-results/api-test-results.json \
+    || exit 1
+  
+sleep 2
+
+curl -v \
+    --user "admin:$ART_PASS" \
+    --upload-file ./results/ux-test-results.html \
+        https://art.daas.digidhamu.com/repository/raw-daas-digidhamu/test-results/ux-test-results.html \
+    || exit 1
+
+sleep 2
+
+curl -v \
+    --user "admin:$ART_PASS" \
+    --upload-file ./results/sec-test-results.html \
+        https://art.daas.digidhamu.com/repository/raw-daas-digidhamu/test-results/sec-test-results.html \
+    || exit 1
+
+sleep 2
+
+curl -v \
+    --user "admin:$ART_PASS" \
+    --upload-file ./results/perf-test-results.txt \
+        https://art.daas.digidhamu.com/repository/raw-daas-digidhamu/test-results/perf-test-results.txt \
+    || exit 1
+
+## Clean up
+rm -f ./results_*
--- sec-test.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Run OWASP ZAP security testing via docker container
-docker run -v $(pwd):/zap/wrk/:rw -t owasp/zap2docker-stable zap-baseline.py -t http://192.168.64.32:32008 -g ./results/gen.conf -r ./results/sec-test-results.html
-
-# Workaround for success message
-echo 'supressing exit code -2'
