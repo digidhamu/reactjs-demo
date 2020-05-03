6337a9932038481b8e011c8acb7dfd943f3cda6b
Dhamodharan Krishnan <dhamukrish@gmail.com>
2020-05-03 10:41:52 +0200

    Updated test-sec

--- test-sec.sh
+++ test-sec.sh
@@ -3,7 +3,12 @@
 kubectl config set-context docker-desktop
 
 # Run OWASP ZAP security testing via docker container
-docker run -v $(pwd):/zap/wrk/:rw -t owasp/zap2docker-stable zap-baseline.py -t http://192.168.64.32:32008 -g ./results/gen.conf -r ./results/sec-test-results.html
+docker run \
+    -v "$(pwd):/zap/wrk/:rw" \
+    -t owasp/zap2docker-stable zap-full-scan.py \
+    -t http://192.168.64.32:32008 \
+    -g ./results/gen.conf \
+    -r ./results/sec-test-results.html
 
 # Workaround for success message
 echo 'supressing exit code -2'
