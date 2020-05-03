db065cd83955ffb220ef3d84a31ed6f87ea4d0c6
Dhamodharan Krishnan <dhamukrish@gmail.com>
2020-05-03 08:04:21 +0200

    Added 2 secs delay for artifacts upload

--- artifact-upload.sh
+++ artifact-upload.sh
@@ -4,13 +4,19 @@ curl -v \
     --user "admin:$1" \
     --upload-file ./results/api-test-results.html https://art.daas.digidhamu.com/repository/raw-daas-digidhamu/test-results/api-test-results.html
 
+sleep 2
+
 curl -v \
     --user "admin:$1" \
     --upload-file ./results/api-test-results.json https://art.daas.digidhamu.com/repository/raw-daas-digidhamu/test-results/api-test-results.json
 
+sleep 2
+
 curl -v \
     --user "admin:$1" \
     --upload-file ./results/perf-test-results.txt https://art.daas.digidhamu.com/repository/raw-daas-digidhamu/test-results/perf-test-results.txt
+
+sleep 2
 
 curl -v \
     --user "admin:$1" \
