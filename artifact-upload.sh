189ad462e0d5cd15c7173776da3bc063768fe459
Dhamodharan Krishnan <dhamukrish@gmail.com>
2020-05-04 18:58:11 +0200

    Updated artifact-upload script

--- artifact-upload.sh
+++ artifact-upload.sh
@@ -4,71 +4,117 @@ export timestamp=`date +%Y-%m-%d"_"%H_%M_%S`
 
 ART_PASS=$1
 
+API_TEST_HTML="api-test-results.html"
+API_TEST_JSON="api-test-results.json"
+FUNC_TEST_MP4="func-test-results.mp4"
+UX_TEST_HTML="ux-test-results.html"
+SEC_TEST_HTML="sec-test-results.html"
+PERF_TEST_TXT="perf-test-results.txt"
+
 tar -czvf \
     ./results_${timestamp}.tar.gz \
     ./results/* \
     || exit 1
 
-curl -v \
-    --user "admin:$ART_PASS" \
-    --http1.1 \
-    -T ./results_${timestamp}.tar.gz \
-        https://art.daas.digidhamu.com/repository/raw-daas-digidhamu/test-results/results_${timestamp}.tar.gz \
-    || exit 1
+if [[ -f "./results_${timestamp}.tar.gz" ]]; then
+    curl \
+        --user "admin:$ART_PASS" \
+        --http1.1 \
+        -T "./results_${timestamp}.tar.gz" \
+            https://art.daas.digidhamu.com/repository/raw-daas-digidhamu/test-results/results_${timestamp}.tar.gz \
+        || exit 1
 
-sleep 2
+    echo "File results_${timestamp}.tar.gz is uploaded"
+    sleep 2
+else
+    echo "File results_${timestamp}.tar.gz is not present"
+fi
 
-curl -v \
-    --user "admin:$ART_PASS" \
-    --http1.1 \
-    -T ./results/api-test-results.html \
-        https://art.daas.digidhamu.com/repository/raw-daas-digidhamu/test-results/api-test-results.html \
-    || exit 1
+if [[ -f "./results/$API_TEST_HTML" ]]; then
+    curl \
+        --user "admin:$ART_PASS" \
+        --http1.1 \
+        -T "./results/$API_TEST_HTML" \
+            https://art.daas.digidhamu.com/repository/raw-daas-digidhamu/test-results/$API_TEST_HTML \
+        || exit 1
 
-cp ./e2e/cypress/videos/spec.js.mp4 ./results/func-test-results.mp4
+    echo "File $API_TEST_HTML is uploaded"    
+    sleep 2
+else
+    echo "File $API_TEST_HTML is not present"
+fi
 
-curl -v \
-    --user "admin:$ART_PASS" \
-    --http1.1 \
-    -T ./results/func-test-results.mp4 \
-        https://art.daas.digidhamu.com/repository/raw-daas-digidhamu/test-results/func-test-results.mp4 \
-    || exit 1
+if [[ -f "./results/$API_TEST_JSON" ]]; then
+    curl \
+        --user "admin:$ART_PASS" \
+        --http1.1 \
+        -T "./results/$API_TEST_JSON" \
+            https://art.daas.digidhamu.com/repository/raw-daas-digidhamu/test-results/$API_TEST_JSON \
+        || exit 1
+    
+    echo "File $API_TEST_JSON is uploaded"
+    sleep 2
+else
+    echo "File $API_TEST_JSON is not present"
+fi
 
-sleep 2
+cp ./e2e/cypress/videos/spec.js.mp4 ./results/$FUNC_TEST_MP4
 
-curl -v \
-    --user "admin:$ART_PASS" \
-    --http1.1 \
-    -T ./results/api-test-results.json \
-        https://art.daas.digidhamu.com/repository/raw-daas-digidhamu/test-results/api-test-results.json \
-    || exit 1
-  
-sleep 2
+if [[ -f "./results/$FUNC_TEST_MP4" ]]; then
+    curl \
+        --user "admin:$ART_PASS" \
+        --http1.1 \
+        -T "./results/$FUNC_TEST_MP4" \
+            https://art.daas.digidhamu.com/repository/raw-daas-digidhamu/test-results/$FUNC_TEST_MP4 \
+        || exit 1
 
-curl -v \
-    --user "admin:$ART_PASS" \
-    --http1.1 \
-    -T ./results/ux-test-results.html \
-        https://art.daas.digidhamu.com/repository/raw-daas-digidhamu/test-results/ux-test-results.html \
-    || exit 1
+    echo "File $FUNC_TEST_MP4 is uploaded"
+    sleep 2
+else
+    echo "File $FUNC_TEST_MP4 is not present"
+fi
 
-sleep 2
+if [[ -f "./results/$UX_TEST_HTML" ]]; then
+    curl \
+        --user "admin:$ART_PASS" \
+        --http1.1 \
+        -T "./results/$UX_TEST_HTML" \
+            https://art.daas.digidhamu.com/repository/raw-daas-digidhamu/test-results/$UX_TEST_HTML \
+        || exit 1
 
-curl -v \
-    --user "admin:$ART_PASS" \
-    --http1.1 \
-    -T ./results/sec-test-results.html \
-        https://art.daas.digidhamu.com/repository/raw-daas-digidhamu/test-results/sec-test-results.html \
-    || exit 1
+    echo "File $UX_TEST_HTML is uploaded"
+    sleep 2
+else
+    echo "File $UX_TEST_HTML is not present"
+fi
 
-sleep 2
+if [[ -f "./results/$SEC_TEST_HTML" ]]; then
+    curl \
+        --user "admin:$ART_PASS" \
+        --http1.1 \
+        -T "./results/$SEC_TEST_HTML" \
+            https://art.daas.digidhamu.com/repository/raw-daas-digidhamu/test-results/$SEC_TEST_HTML \
+        || exit 1
 
-curl -v \
-    --user "admin:$ART_PASS" \
-    --http1.1 \
-    -T ./results/perf-test-results.txt \
-        https://art.daas.digidhamu.com/repository/raw-daas-digidhamu/test-results/perf-test-results.txt \
-    || exit 1
+    echo "File $SEC_TEST_HTML is uploaded"
+    sleep 2
+else
+    echo "File $SEC_TEST_HTML is not present"
+fi
+
+if [[ -f "./results/$PERF_TEST_TXT" ]]; then
+    curl \
+        --user "admin:$ART_PASS" \
+        --http1.1 \
+        -T "./results/$PERF_TEST_TXT" \
+            https://art.daas.digidhamu.com/repository/raw-daas-digidhamu/test-results/$PERF_TEST_TXT \
+        || exit 1
+
+    echo "File $PERF_TEST_TXT is uploaded"
+    sleep 2
+else
+    echo "File $PERF_TEST_TXT is not present"
+fi
 
 ## Clean up
 rm -f ./results_*
