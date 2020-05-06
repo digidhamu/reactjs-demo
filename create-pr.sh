2314436c2f87150ea038fd0a69fa860377052c70
Dhamodharan Krishnan <dhamukrish@gmail.com>
2020-05-06 12:46:35 +0200

    Updated for github token

--- create-pr.sh
+++ create-pr.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 
-if [[ -z $1 ]]; then
-    echo Required access details is not supplied
-    echo "eg. ./update-pr.sh GITHUB_TOKEN"
-    exit 1
-fi
+# if [[ -z $1 ]]; then
+#     echo Required access details is not supplied
+#     echo "eg. ./update-pr.sh GITHUB_TOKEN"
+#     exit 1
+# fi
 
 GITHUB_REPO=reactjs-demo
-GITHUB_TOKEN=$1
+# GITHUB_TOKEN=$1
+
+GITHUB_TOKEN=$(curl -n -skg "https://secretmanager.googleapis.com/v1/projects/202626771609/secrets/github-token/versions/1:access" \
+    --request "GET" \
+    --header "authorization: Bearer $(gcloud auth print-access-token)" \
+    --header "content-type: application/json" \
+    --header "x-goog-user-project: digidhamu-k8s" \
+    | jq -r ".payload.data" | base64 --decode)
 
 ## Create Pull Request
 curl -X "POST" "https://api.github.com/repos/digidhamu/${GITHUB_REPO}/pulls" \
