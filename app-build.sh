aa6d40f09397ca91182c442c5c9304ba01a1ec9c
Dhamodharan Krishnan <dhamukrish@gmail.com>
2020-05-06 17:34:54 +0200

    Added no space to the message

--- app-build.sh
+++ app-build.sh
@@ -2,15 +2,15 @@
 
 source ./parse-flowref.sh $1
 
-./post-progress.sh $STAGE_ID $PIPELINE_ID "Setting up context" 10
+./post-progress.sh $STAGE_ID $PIPELINE_ID "setting-up-context" 10
 
 kubectl config set-context docker-desktop
 
-./post-progress.sh $STAGE_ID $PIPELINE_ID "Build the docker image" 50
+./post-progress.sh $STAGE_ID $PIPELINE_ID "build-the-docker-image" 50
 docker build -t reactjs-demo .
 
-./post-progress.sh $STAGE_ID $PIPELINE_ID "Taging build image" 80
+./post-progress.sh $STAGE_ID $PIPELINE_ID "taging-build-image" 80
 docker tag reactjs-demo dcr.daas.digidhamu.com/reactjs-demo:latest
 
-./post-progress.sh $STAGE_ID $PIPELINE_ID "Push the docker images" 90
+./post-progress.sh $STAGE_ID $PIPELINE_ID "push-the-docker-images" 90
 docker push dcr.daas.digidhamu.com/reactjs-demo:latest
