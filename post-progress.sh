e3f411f5f10eea5e20fae6696d8f1f969fc146db
Dhamodharan Krishnan <dhamukrish@gmail.com>
2020-05-06 17:19:08 +0200

    Updated message parsing

--- post-progress.sh
+++ post-progress.sh
@@ -2,16 +2,16 @@
 
 set +xe
 
-curl -s -X "POST" "https://ctl.daas.digidhamu.com/jcl-script-hook" \
-	 -H 'Content-Type: application/json; charset=utf-8' \
-	 -d $'{
-		"msgStageProgress": {
-			"stage_id": '${1}',
-			"pipeline_id": '${2}',
-			"step_status": "'${3}'",
-			"progress_status": '${4}'
-		}
-	 }'
+curl -X "POST" "https://ctl.daas.digidhamu.com/jcl-script-hook" \
+     -H 'Content-Type: application/json; charset=utf-8' \
+     -d $'{
+  "msgStageProgress": {
+    "stage_id": '$1',
+    "pipeline_id": '$2',
+    "step_status": '\"$3\"',
+    "progress_status": '$4'
+  }
+}'
 
 echo
 
