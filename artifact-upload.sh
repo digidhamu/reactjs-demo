e8edcc8dc62ac3997849941469e2c92c145d3e7c
Dhamodharan Krishnan <dhamukrish@gmail.com>
2020-05-09 20:55:27 +0200

    Added the release tagging

--- artifact-upload.sh
+++ artifact-upload.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 
-source ./parse-flowref.sh $1
+# source ./parse-flowref.sh $1
 
 # Get the release tag
-echo `curl "https://ctl.daas.digidhamu.com/release-next-tag?stage_uuid=$STAGE_UUID"` | jq .
+TAG_JSON=`curl -skg "https://ctl.daas.digidhamu.com/release-next-tag?stage_uuid=$STAGE_UUID"`
+
+RELEASE_VERSION=`echo $TAG_JSON | jq --raw-output .version`
+TAG_LABEL=`echo $TAG_JSON | jq --raw-output .auto_labelling`
+BUILD_NUMBER=`echo $TAG_JSON | jq --raw-output .run_count`
+
+IMAGE_TAGGING="v$RELEASE_VERSION-$TAG_LABEL.$BUILD_NUMBER"
+echo $IMAGE_TAGGING
 
 ./post-progress.sh "$STAGE_UUID" "Taging releasing label" 50
-docker tag reactjs-demo dcr.daas.digidhamu.com/reactjs-demo:latest
+docker tag reactjs-demo dcr.daas.digidhamu.com/reactjs-demo:$IMAGE_TAGGING
 
 ./post-progress.sh "$STAGE_UUID" "Push the labelled image" 90
-docker push dcr.daas.digidhamu.com/reactjs-demo:latest
+docker push dcr.daas.digidhamu.com/reactjs-demo:$IMAGE_TAGGING
