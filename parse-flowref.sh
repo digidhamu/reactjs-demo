984c80eefeac5a2ab819c194c58bd4e1977fb34d
Dhamodharan Krishnan <dhamukrish@gmail.com>
2020-05-09 20:34:46 +0200

    Updated for stage_uuid

--- parse-flowref.sh
+++ parse-flowref.sh
@@ -4,14 +4,16 @@ FLOW_REF=$1
 
 echo $FLOW_REF
 
-# FLOW_REF='{&quot;flow_id&quot;:&quot;8eec3774-9b94-437f-a0c7-582334118921&quot;,&quot;pipeline_id&quot;:2,&quot;flow_sequence&quot;:2,&quot;stage_id&quot;:8}'
-# echo '{"flow_id":"8eec3774-9b94-437f-a0c7-582334118921","pipeline_id":2,"flow_sequence":2,"stage_id":8}' | jq .
+## Input
+# {&quot;flow_id&quot;:&quot;1edf38ca-f03f-482f-a5d9-7dea5d8533d7&quot;,&quot;flow_sequence&quot;:4,&quot;stage_uuid&quot;:&quot;cfecbe6a-0376-4aaa-a48b-f56e4f76af32&quot;}'
 
 FORMATED_JSON=`echo $FLOW_REF | sed 's/&quot;/\"/g'`
 echo $FORMATED_JSON
 
+## Output
+# {"flow_id":"1edf38ca-f03f-482f-a5d9-7dea5d8533d7","flow_sequence":4,"stage_uuid":"cfecbe6a-0376-4aaa-a48b-f56e4f76af32"}
+
 ## --raw-output will remove double quote
 export FLOW_ID=`echo $FORMATED_JSON | jq --raw-output .flow_id`
-export PIPELINE_ID=`echo $FORMATED_JSON | jq --raw-output .pipeline_id`
 export FLOW_SEQUENCE=`echo $FORMATED_JSON | jq --raw-output .flow_sequence`
-export STAGE_ID=`echo $FORMATED_JSON | jq --raw-output .stage_id`
+export STAGE_UUID=`echo $FORMATED_JSON | jq --raw-output .stage_uuid`
