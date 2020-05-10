768531b064b7d92c3e6f0dc764bffa8e5e112a83
Dhamodharan Krishnan <dhamukrish@gmail.com>
2020-05-10 07:34:21 +0200

    Updated script for modularity and version

--- artifact-upload.sh
+++ artifact-upload.sh
@@ -1,11 +1,42 @@
 #!/bin/bash
 
-source ./parse-flowref.sh $1
+set -xo errexit # exit immediately on error
 
+source ./set-script-vars.sh $1
 source ./get-release-tag.sh $STAGE_UUID
 
+##
+# Docker Image Upload
+##
 ./post-progress.sh $STAGE_UUID "Taging releasing label" 50
 docker tag reactjs-demo dcr.daas.digidhamu.com/reactjs-demo:$RELEASE_TAG
 
 ./post-progress.sh $STAGE_UUID "Push the labelled image" 90
 docker push dcr.daas.digidhamu.com/reactjs-demo:$RELEASE_TAG
+
+##
+# API Test Result Upload
+##
+API_TEST_JSON_RELEASE_TAG="api-test-results_$RELEASE_TAG.json"
+API_TEST_HTML_RELEASE_TAG="api-test-results_$RELEASE_TAG.html"
+
+cp results/$API_TEST_JSON results/$API_TEST_JSON_RELEASE_TAG
+cp results/$API_TEST_HTML results/$API_TEST_HTML_RELEASE_TAG
+
+curl \
+    --user "$ART_ACCESS" \
+    --http1.1 \
+    -T ./results/$API_TEST_JSON_RELEASE_TAG \
+        $ART_SERVER/$API_TEST_JSON_RELEASE_TAG \
+    || exit 1
+
+echo File $API_TEST_JSON_RELEASE_TAG is uploaded
+
+curl \
+    --user "$ART_ACCESS" \
+    --http1.1 \
+    -T ./results/$API_TEST_HTML_RELEASE_TAG \
+        $ART_SERVER/$API_TEST_HTML_RELEASE_TAG \
+    || exit 1
+
+echo File $API_TEST_HTML_RELEASE_TAG is uploaded
--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           Home
         </Button>
         <p>
-          Version v1.1.0-alpha.12
+          Version v1.1.0-alpha.13
         </p>
       </header>
     </div>
