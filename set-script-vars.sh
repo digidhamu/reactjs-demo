adce2b4810f5d54f8f54e6b9803b5c2cc380ec4a
Dhamodharan Krishnan <dhamukrish@gmail.com>
2020-05-10 07:53:25 +0200

    Updated ux test and version

--- set-script-vars.sh
+++ set-script-vars.sh
@@ -3,6 +3,8 @@
 export API_TEST_JSON="api-test-results.json"
 export API_TEST_HTML="api-test-results.html"
 export FUNC_TEST_MP4="func-test-results.mp4"
+export UX_TEST_HTML="ux-test-results.html"
+
 export ART_SERVER="https://art.daas.digidhamu.com/repository/raw-daas-digidhamu/test-results"
 
 FLOW_REF=$1
--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           Home
         </Button>
         <p>
-          Version v1.1.0-alpha.14
+          Version v1.1.0-alpha.15
         </p>
       </header>
     </div>
