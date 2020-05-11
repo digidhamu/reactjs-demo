3e2b9b7c4e1a40a4174c8e43f8ebd4df59436b33
Dhamodharan Krishnan <dhamukrish@gmail.com>
2020-05-11 10:07:41 +0200

    Added release name as part of tag

--- get-release-tag.sh
+++ get-release-tag.sh
@@ -5,9 +5,10 @@ STAGE_UUID=$1
 # Get the release tag
 TAG_JSON=`curl -skg "https://ctl.daas.digidhamu.com/release-next-tag?stage_uuid=$STAGE_UUID"`
 
+RELEASE_NAME==`echo $TAG_JSON | jq --raw-output .name`
 RELEASE_VERSION=`echo $TAG_JSON | jq --raw-output .version`
 TAG_LABEL=`echo $TAG_JSON | jq --raw-output .auto_labelling`
 BUILD_NUMBER=`echo $TAG_JSON | jq --raw-output .run_count`
 
-export RELEASE_TAG="v$RELEASE_VERSION-$TAG_LABEL.$BUILD_NUMBER"
+export RELEASE_TAG="$RELEASE_NAME_v$RELEASE_VERSION-$TAG_LABEL.$BUILD_NUMBER"
 echo $RELEASE_TAG
--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           Home
         </Button>
         <p>
-          Version v1.1.0-alpha.1
+          Version v1.1.0-alpha.5
         </p>
       </header>
     </div>
