ea8b03d8372d7582a06840364c9048bcbc1e47a8
Dhamodharan Krishnan <dhamukrish@gmail.com>
2020-12-23 21:59:12 +0530

    Reverted to delete deployment

--- app-deploy.sh
+++ app-deploy.sh
@@ -7,9 +7,9 @@ source ./set-script-vars.sh $1
 ./post-progress.sh $STAGE_UUID "Setting up context" 10
 kubectl config set-context minikube
 
-# ./post-progress.sh $STAGE_UUID "Uninstalling app" 20
-# kubectl delete -f $APP_NAME.yaml
-# sleep 10
+./post-progress.sh $STAGE_UUID "Uninstalling app" 20
+kubectl delete -f $APP_NAME.yaml
+sleep 10
 
 ./post-progress.sh $STAGE_UUID "Installing app" 70
 kubectl apply -f $APP_NAME.yaml
