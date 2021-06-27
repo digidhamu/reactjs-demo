eb94e19c06f78899da2dce9ad10a2a83d37862aa
Dhamodharan Krishnan <dhamukrish@gmail.com>
2021-06-27 08:34:57 +0530

    Removed app delete

--- app-deploy.sh
+++ app-deploy.sh
@@ -8,8 +8,8 @@ source ./set-script-vars.sh $1
 # kubectl config set-context minikube
 
 ./post-progress.sh $STAGE_UUID "Uninstalling app" 20
-kubectl delete -f $APP_NAME.yaml
-sleep 10
+# kubectl delete -f $APP_NAME.yaml
+# sleep 10
 
 ./post-progress.sh $STAGE_UUID "Installing app" 70
 kubectl apply -f $APP_NAME.yaml
