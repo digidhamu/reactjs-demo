e569be5d07fbb60eae4a277421cb6face707f31e
Dhamodharan Krishnan <dhamukrish@gmail.com>
2021-06-29 07:51:37 +0530

    Fixed UAT image deployment

--- deploy-prod.sh
+++ deploy-prod.sh
@@ -5,12 +5,12 @@ set -o errexit # exit immediately on error
 source ./set-script-vars.sh $1
 
 ./post-progress.sh $STAGE_UUID "Setting up context" 10
-kubectl config set-context minikube
+# kubectl config set-context minikube
 
 ./post-progress.sh $STAGE_UUID "Uninstalling app" 20
-kubectl delete -f $APP_NAME.yaml
+kubectl delete -f $APP_NAME.yaml --ignore-not-found=true
 sleep 10
 
 ./post-progress.sh $STAGE_UUID "Installing app" 70
-kubectl apply -f $APP_NAME.yaml
+kubectl create -f $APP_NAME.yaml
 sleep 10
